a633471cfc0a6ed581260cd88fe9a0f0a78783e0
kkozersk <01158764@pw.edu.pl>
2024-05-26 17:02:46 +0200

    Fixed redis inserts - only useful records are being kept

--- redis/initial_insert.py
+++ redis/initial_insert.py
@@ -3,6 +3,5 @@ import csv
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 with open('../data/in_db.edges') as fp:
     for row in csv.reader(fp):
-        key = 'userProducts' + row[0]
-        value = row[1] + ':' + row[2]
-        r.sadd(key, value)
+        if float(row[2]) > 3.0:
+            r.sadd('userProducts:' + row[0], row[1])
